eaa6db266a1e9f7970e342aecf72a76507803232
Albin Restorp <albin@restorp.se>
2025-04-20 00:32:49 +0200

    "cooking..."

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,4 +1,9 @@
 import type { Node, BuiltInNode } from "@xyflow/react";
+import { RecipeId, RecipeOptionValue } from "../data/recipes";
 
 export type PositionLoggerNode = Node<{ label: string }, "position-logger">;
-export type AppNode = BuiltInNode | PositionLoggerNode;
+export type MachineNode<T extends RecipeId> = Node<
+  { recipeId: T; optionValues?: RecipeOptionValue<T> },
+  "machine-node"
+>;
+export type AppNode = BuiltInNode | PositionLoggerNode | MachineNode<RecipeId>;
