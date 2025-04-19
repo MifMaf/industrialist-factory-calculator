a20b442efa72aa19f119ca2f22c398b86c02296f
MifMaf <187258417+MifMaf@users.noreply.github.com>
2025-04-20 00:32:49 +0200

    "cooking..."

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,26 +1,34 @@
-import type { NodeTypes } from '@xyflow/react';
+import type { NodeTypes } from "@xyflow/react";
 
-import { PositionLoggerNode } from './PositionLoggerNode';
-import { AppNode } from './types';
+import { PositionLoggerNode } from "./PositionLoggerNode";
+import { AppNode } from "./types";
+import { MachineNode } from "./MachineNode";
 
 export const initialNodes: AppNode[] = [
-  { id: 'a', type: 'input', position: { x: 0, y: 0 }, data: { label: 'wire' } },
+  { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
   {
-    id: 'b',
-    type: 'position-logger',
+    id: "b",
+    type: "position-logger",
     position: { x: -100, y: 100 },
-    data: { label: 'drag me!' },
+    data: { label: "drag me!" },
   },
-  { id: 'c', position: { x: 100, y: 100 }, data: { label: 'your ideas' } },
+  { id: "c", position: { x: 100, y: 100 }, data: { label: "your ideas" } },
   {
-    id: 'd',
-    type: 'output',
+    id: "d",
+    type: "output",
     position: { x: 0, y: 200 },
-    data: { label: 'with React Flow' },
+    data: { label: "with React Flow" },
+  },
+  {
+    id: "e",
+    type: "machine-node",
+    position: { x: -100, y: -100 },
+    data: { recipeId: "water-pump2" },
   },
 ];
 
 export const nodeTypes = {
-  'position-logger': PositionLoggerNode,
+  "position-logger": PositionLoggerNode,
   // Add any of your custom nodes here!
+  "machine-node": MachineNode,
 } satisfies NodeTypes;
