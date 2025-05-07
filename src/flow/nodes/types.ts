bebbbc7bcec4c6ac6a0c6042e69691d06fd872a5
MifMaf <187258417+MifMaf@users.noreply.github.com>
2025-05-07 17:33:20 +0200

    changed "machine node" to "recipe node"

--- src/flow/nodes/types.ts
+++ src/flow/nodes/types.ts
@@ -2,13 +2,13 @@ import type { Node, BuiltInNode } from "@xyflow/react";
 import { RecipeId, RecipeOptionValue } from "../../data/recipes";
 
 export type PositionLoggerNode = Node<{ label: string }, "position-logger">;
-export type MachineNode<T extends RecipeId> = Node<
+export type RecipeNode<T extends RecipeId> = Node<
   {
     recipeId: T;
     optionValues?: RecipeOptionValue<T>;
     fixed: boolean;
     machineCount: number | null;
   },
-  "machine-node"
+  "recipe-node"
 >;
-export type AppNode = BuiltInNode | PositionLoggerNode | MachineNode<RecipeId>;
+export type AppNode = BuiltInNode | PositionLoggerNode | RecipeNode<RecipeId>;
